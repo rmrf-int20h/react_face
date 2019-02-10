03ba7922a84188a1d57f92ad4eae37a0c26adb28
Mykola <mykola.lev@gmail.com>
2019-02-10 22:38:29 +0200

    react app

---
+++ precache-manifest.faa2fb90898da67767ac2456fc00aafa.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "ba26a09a584e373fe20c736abc9da57d",
+    "url": "/static/media/fear.ba26a09a.png"
+  },
+  {
+    "revision": "edbe70c714eb18b98e87",
+    "url": "/static/css/main.3cfd2544.chunk.css"
+  },
+  {
+    "revision": "a85c5d61634ac233f659",
+    "url": "/static/js/1.a85c5d61.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9da9333786053ade792e4b7c45951df5",
+    "url": "/static/media/anger.9da93337.png"
+  },
+  {
+    "revision": "edbe70c714eb18b98e87",
+    "url": "/static/js/main.edbe70c7.chunk.js"
+  },
+  {
+    "revision": "626f72c87465f69004d623d9f1b11684",
+    "url": "/static/media/happiness.626f72c8.png"
+  },
+  {
+    "revision": "e61d034f887ed08c7ee6e72c34386409",
+    "url": "/static/media/neutral.e61d034f.png"
+  },
+  {
+    "revision": "a8e83fdfdb51a5352747161a384879aa",
+    "url": "/static/media/sadness.a8e83fdf.png"
+  },
+  {
+    "revision": "b96c9df27203c6abf6eaa4dc3cc81a62",
+    "url": "/static/media/surprise.b96c9df2.png"
+  },
+  {
+    "revision": "3bbb611b9f840474e73c5e692f8dbce3",
+    "url": "/index.html"
+  }
+];
